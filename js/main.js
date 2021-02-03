e6503450daad922948e0e1e23a0e0cd85cf6246b
alex-lbv <aal940722@gmail.com>
2021-02-03 22:41:19 +0300

    сделал правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 7;
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 10;
+const MIN_PRICE = 100000;
+const MAX_PRICE = 1000000;
+const MIN_USER_COUNT = 1;
+const MAX_USER_COUNT = 8;
+const SIMILAR_OFFER_COUNT = 10;
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTO_LINKS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+const DESCRIPTION = 'Описание помещения';
+const OfferType = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalow': 'Бунгало',
+};
+
 const getRandomNumber = (min, max) => {
   if (min < 0 || max < 0) {
     throw new Error('Отрицательные значения запрещены');
@@ -27,9 +62,7 @@ const getRandomFloatNumber = (min, max, digits = 1) => {
   return random.toFixed(digits);
 };
 
-const getRandomArrayElement = (array) => {
-  return array[getRandomNumber(0, array.length - 1)];
-};
+const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
 
 const getShuffledArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -50,73 +83,38 @@ const getRandomLengthArray = (array) => {
   return randomLengthArray;
 };
 
-
-const TYPE_AD = {
-  'palace' : 'Дворец',
-  'flat' : 'Квартира',
-  'house' : 'Дом',
-  'bungalow' : 'Бунгало',
-};
-const CHECKIN_AND_CHECKOUT_AD = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-const FEATURES_AD = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-const PHOTOS_AD = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
-const SIMILAR_ADS_COUNT = 10;
-
-const createAd = () => {
+const createOffer = () => {
   const location = {
-    x : getRandomFloatNumber(35.65000, 35.70000, 5),
-    y : getRandomFloatNumber(139.70000, 139.80000, 5),
+    x: getRandomFloatNumber(35.65000, 35.70000, 5),
+    y: getRandomFloatNumber(139.70000, 139.80000, 5),
   };
-  const type = getRandomArrayElement(Object.keys(TYPE_AD));
-  const MIN_ROOMS = 1;
-  const MAX_ROOMS = 7;
-  const MIN_GUESTS = 1;
-  const MAX_GUESTS = 10;
-  const MIN_PRICE = 100000;
-  const MAX_PRICE = 1000000;
-  const MIN_USER_COUNT = 1;
-  const MAX_USER_COUNT = 8;
+  const type = getRandomArrayElement(Object.keys(OfferType));
 
   return {
-    author : {
-      avatar : `img/avatars/user0${getRandomNumber(MIN_USER_COUNT,MAX_USER_COUNT)}.png`,
+    author: {
+      avatar: `img/avatars/user0${getRandomNumber(MIN_USER_COUNT, MAX_USER_COUNT)}.png`,
     },
-    offer : {
-      title : Object.values(TYPE_AD[type]).join(''),
-      address : `${location.x}, ${location.y}`,
-      price : getRandomNumber(MIN_PRICE, MAX_PRICE),
-      type : type,
-      rooms : getRandomNumber(MIN_ROOMS, MAX_ROOMS),
-      guests : getRandomNumber(MIN_GUESTS, MAX_GUESTS),
-      checkin : getRandomArrayElement(CHECKIN_AND_CHECKOUT_AD),
-      checkout : getRandomArrayElement(CHECKIN_AND_CHECKOUT_AD),
-      features : getRandomLengthArray(FEATURES_AD),
-      description : 'Описание помещения',
-      photos : getRandomLengthArray(PHOTOS_AD),
+    offer: {
+      title: Object.values(OfferType[type]).join(''),
+      address: `${location.x}, ${location.y}`,
+      price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+      type: type,
+      rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+      checkin: getRandomArrayElement(TIMES),
+      checkout: getRandomArrayElement(TIMES),
+      features: getRandomLengthArray(FEATURES),
+      description: DESCRIPTION,
+      photos: getRandomLengthArray(PHOTO_LINKS),
     },
-    location : {
-      x : location.x,
-      y : location.y,
+    location: {
+      x: location.x,
+      y: location.y,
     },
   }
 };
 
-const similarAds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAd());
+const similarOffer = new Array(SIMILAR_OFFER_COUNT).fill(null).map(createOffer);
 
-const getSimilarAds = () => similarAds;
-getSimilarAds();
+const getSimilarOffer = () => similarOffer;
+getSimilarOffer();
