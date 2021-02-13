43292d009a66f5a85a99584ffc653aaec35913aa
alex-lbv <aal940722@gmail.com>
2021-02-13 17:51:54 +0300

    Добавил модуль генерации разметки

---
+++ js/render-offer.js
@@ -0,0 +1,72 @@
+import {similarOffers, OfferType} from './data.js';
+import {makeElement, changeEndOfWords} from './util.js';
+
+const similarListElement = document.querySelector('#map-canvas');
+const similarOfferTemplate = document.querySelector('#card').content.querySelector('.popup');
+const similarListFragment = document.createDocumentFragment();
+
+similarOffers.forEach(({offer, author}) => {
+  const offerElement = similarOfferTemplate.cloneNode(true);
+
+  const offerTitle = offerElement.querySelector('.popup__title');
+  (offer.title) ? offerTitle.textContent = offer.title : offerTitle.remove();
+
+  const offerAddress = offerElement.querySelector('.popup__text--address');
+  (offer.address) ? offerAddress.textContent = offer.address : offerAddress.remove();
+
+  const offerPrice = offerElement.querySelector('.popup__text--price');
+  (offer.price) ? offerPrice.textContent = `${offer.price} ₽/ночь` : offerPrice.remove();
+
+  const offerType = offerElement.querySelector('.popup__type');
+  (offer.type) ? offerType.textContent = Object.values(OfferType[offer.type]).join('')
+    : offerType.remove();
+
+  const offerRoomsAndGuests = offerElement.querySelector('.popup__text--capacity');
+  if (offer.rooms && offer.guests) {
+    offerRoomsAndGuests.textContent =
+      `${offer.rooms} ${changeEndOfWords(offer.rooms, ['комната', 'комнаты', 'комнат'])}
+    для ${offer.guests} ${changeEndOfWords(offer.guests, ['гостя', 'гостей', 'гостей'])}`;
+  } else offerRoomsAndGuests.remove();
+
+  const offerTime = offerElement.querySelector('.popup__text--time');
+  (offer.checkin && offer.checkout) ? offerTime.textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`
+    : offerTime.remove();
+
+  const offerDescription = offerElement.querySelector('.popup__description');
+  (offer.description) ? offerDescription.textContent = offer.description
+    : offerDescription.remove();
+
+  const offerAvatar = offerElement.querySelector('.popup__avatar');
+  (author.avatar) ? offerAvatar.src = author.avatar : offerAvatar.remove();
+
+  const photoList = offerElement.querySelector('.popup__photos');
+  if (offer.photos) {
+    photoList.innerHTML = '';
+
+    for (let i = 0; i <= offer.photos.length - 1; i++) {
+      const photo = makeElement('img', 'popup__photo', false, offer.photos[i]);
+      photo.width = 45;
+      photo.height = 40;
+      photo.alt = 'Фотография жилья';
+      photoList.appendChild(photo);
+    }
+  } else photoList.remove();
+
+  const featuresList = offerElement.querySelector('.popup__features');
+  if (offer.features) {
+    featuresList.innerHTML = '';
+
+    for (let i = 0; i <= offer.features.length - 1; i++) {
+      const feature = makeElement('li', 'popup__feature', `popup__feature--${offer.features[i]}`);
+      featuresList.appendChild(feature);
+    }
+  } else featuresList.remove();
+
+  similarListFragment.appendChild(offerElement);
+});
+
+export const renderOffer = (numberOffer) => {
+  similarListElement.appendChild(similarListFragment.children[numberOffer]);
+};
+
+renderOffer(0);
--- js/util.js
+++ js/util.js
@@ -45,3 +45,22 @@ export const getRandomLengthArray = (array) => {
 
   return randomLengthArray;
 };
+
+export const makeElement = (tagName, className, modifier, source) => {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+
+  if (modifier) {
+    element.classList.add(modifier);
+  }
+
+  if (source) {
+    element.src = source;
+  }
+
+  return element;
+};
+
+export const changeEndOfWords = (number, words) => {
+  return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+};
